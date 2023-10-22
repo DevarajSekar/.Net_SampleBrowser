5172d5722b5fe3d856a06c012dac956728cdf167
DevarajSekar <officiallydevaraj@gmail.com>
2023-10-22 23:52:15 +0530

    Palindrome

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindrome Sample");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Enter Some Sample Text");
+            string str = Console.ReadLine();
+            var revStr = string.Empty;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                revStr += str[i];
+            }
+            if (revStr.ToLower() == str.ToLower())
+            {
+                Console.WriteLine("It is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Palindrome");
+            }
+            Console.ReadLine();
+        }
+    }
+}
